4b7c9098465770b90ceb3bd328d140d4ecb385e4
justin terblanche <jterblanche7@gmail.com>
2023-05-30 17:08:29 +0200

    Seed file created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+user1 = User.create!(
+  email: "user1@example.com",
+  password: "password"
+)
+user2 = User.create!(
+  email: "user2@example.com",
+  password: "password"
+)
+user3 = User.create!(
+  email: "user3@example.com",
+  password: "password"
+)
+sneaker_1 = Sneaker.create!(
+  title: "Air Jordan 1 Retro High OG",
+  brand: "Nike",
+  model: "Air Jordan 1",
+  description: "The Air Jordan 1 Retro High OG returns the legendary silhouette to its most accurate-to-original shape and height.",
+  size: 42,
+  price: 160,
+  status: 1,
+  user_id: user1.id
+)
+sneaker_2 = Sneaker.create!(
+  title: "Yeezy Boost 350 V2",
+  brand: "Adidas",
+  model: "Yeezy Boost 350",
+  description: "The Yeezy Boost 350 V2 'Zebra' showcases a dual-tone Primeknit upper in white and core black.",
+  size: 43,
+  price: 220,
+  status: 0,
+  user_id: user2.id
+)
+sneaker_3 = Sneaker.create!(
+  title: 'Stan Smith',
+  brand: "Adidas",
+  model: "Stan Smith",
+  description: "With a clean and minimalist look, the Adidas Stan Smith shoes keep the essence of the 1971 original.",
+  size: 44,
+  price: 80,
+  status: 1,
+  user_id: user3.id
+)
